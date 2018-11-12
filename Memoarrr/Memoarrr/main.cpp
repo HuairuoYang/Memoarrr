fd119b6f1e803714cf8e6e79c9c8af50e3a35603
Huairuo <yvincent74@gmail.com>
2018-11-12 13:20:55 -0500

    creat

---
+++ Memoarrr/Memoarrr/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Memoarrr
+//
+//  Created by Huairuo Yang on 2018-11-12.
+//  Copyright © 2018 Huairuo Yang(7895717). All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
