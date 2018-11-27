2e22b1f7661191c41ae5ec740ee001fd02903f8c
Huairuo <yvincent74@gmail.com>
2018-11-27 15:50:01 -0500

    update

--- Memoarrr/Memoarrr/CardDeck.hpp
+++ Memoarrr/Memoarrr/CardDeck.hpp
@@ -11,13 +11,18 @@
 
 #include <stdio.h>
 #include "Deck.hpp"
+#include "Card.hpp"
 //Design a class CardDeck derived from Deck<Card>.
 //• static CardDeck& make_CardDeck() is the only public method for CardDeck. The method has to always return the same CardDeck during the execution of the program.
 //An object of type CardDeck can not be copied or assigned, and it has no public constructor.
 
-class CardDeck{
+class CardDeck : public Deck<Card> {
     static Deck myDeck;
     static CardDeck& make_CardDeck();
+    
+    CardDeck();
+    CardDeck(const CardDeck&) = delete;
+    CardDeck& operator= ( const CardDeck & ) = delete;
 };
 
 #endif /* CardDeck_hpp */
--- Memoarrr/Memoarrr/Deck.hpp
+++ Memoarrr/Memoarrr/Deck.hpp
@@ -10,16 +10,34 @@
 #define Deck_hpp
 
 #include <stdio.h>
+#include <vector>
 
 //Design a class Deck<C> as an abstract factory class that will be used to create a set of cards or a set of rewards. The type parameter <C> is intended to be one of {Card|Reward}. The class will need the following methods:
 //• void shuffle() shuffles the cards in the deck. You must use the function std::random_shuffle from the standard template library.
 //• C* getNext() returns the next card or reward by pointer. Will return nullptr if no more cards or rewards are available.
 //• bool isEmpty() const returns true if the deck is empty.
 
-class Deck{
-    void shuffle();
-    bool isEmpty();
-    //C* getNext();
+template <typename C>
+class Deck
+{
+    std::vector<C*> deck;
+    int iter = 0;
+    
+public:
+    void shuffle(){
+        std::random_shuffle(deck.begin(), deck.end());
+    };
+    bool isEmpty(){return deck.empty();}
+    
+    C* getNext(){
+        if(isEmpty()){
+            return nullptr;
+        } else {
+            C* card = deck.at(iter);
+            iter++;
+            return card;
+        }
+    };
 };
 
 #endif /* Deck_hpp */
